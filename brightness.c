9fddbe0cd6a4b0ce5a4c8bdd2262df2c49a9b8bb
Mickey Vänskä <mvaenskae@gmail.com>
2015-04-29 11:10:06 +0200

    starting new version with added functionality for relative brightness setting

--- brightness.c
+++ brightness.c
@@ -1,7 +1,21 @@
-#include <stdlib.h>
+#include <errno.h>
 #include <limits.h>
 #include <stdio.h>
-#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct data {
+        long brightness;
+        char sign;
+};
+
+const char *p_brightness = "/sys/class/backlight/nv_backlight/brightness";
+
+//void absolute_brightness();
+//void relative_brightness(FILE, int pm, char *);
+int process_input(int, char *, struct data *);
+//void set_brightness();
+//int get_brightness();
 
 /*
  ** This code is based on the manpages on strtol. It removed an atoi() call
@@ -11,40 +25,25 @@
  */
 int main(int argc, char *argv[])
 {
-        const int base = 10;
-        char *endptr, *str;
-        const char *p_brightness = "/sys/class/backlight/nv_backlight/brightness";
-        FILE *f_brightness = NULL;
-        char *c_brightness;
-        long brightness = 0;
+        struct data *brgt = malloc(sizeof(brgt));
+        memset(brgt, 0, sizeof(*brgt));
 
         if (argc != 2) {
                 fprintf(stderr, "Usage: %s str\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
 
-        str = argv[1]; 
-        errno = 0;    /* To distinguish success/failure after call */
-        brightness = strtol(str, &endptr, base);
-        
-        /* Check for various possible errors */
-        if ((errno == ERANGE &&
-                        (brightness == LONG_MAX || brightness == LONG_MIN))
-                        || (errno != 0 && brightness == 0)) {
-                perror("strtol");
-                exit(EXIT_FAILURE);
-        }
-        
-        if (endptr == str) {
-                fprintf(stderr, "No digits were found\n");
-                exit(EXIT_FAILURE);
+        process_input(argc, argv[1], brgt);
+
+        if (brgt->sign != '\0') {
+        /* We have a relative value to set */
+                printf("Relative value\n");
+        } else {
+        /* We have an absolute value */
+                printf("Absolute value\n");
         }
 
-        if (brightness > 100) {
-                brightness = 100;
-        } else if (brightness < 1) {
-                brightness = 1;
-        }
+        /* Convert value to string */
 
         /*
         f_brightness = fopen(p_brightness, "rb");
@@ -67,7 +66,7 @@ int main(int argc, char *argv[])
         free(c_brightness);
         c_brigthness = NULL;
         */
-        f_brightness = fopen(p_brightness, "wb");
+        /*f_brightness = fopen(p_brightness, "wb");
 
         if (!f_brightness) {
                 perror("fopen");
@@ -87,12 +86,59 @@ int main(int argc, char *argv[])
         printf("Wrote new brightness value %s!\n", c_brightness);
         //free(c_brightness);
         //c_brightness = NULL;
+        */
 
         /* WRITE VALUE INSIDE FILE at /sys/class/backlight/nv_backlight/brightness */
         
+        exit(EXIT_SUCCESS);
+}
+
+/*
+ ** This function is responsible for reading in the stdin after it has been
+ ** guaranteed to be of the correct format. It will parse the string and store
+ ** extracted values within struct data before returning.
+ */
+int process_input(int argc, char *argv, struct data *store)
+{
+        const int base = 10;
+        char *endptr, *str;
+        long val = 0;
+
+        str = argv;
+        errno = 0;    /* To distinguish success/failure after call */
+        val = strtol(str, &endptr, base);
+
+        /* Check for various possible errors */
+        if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
+                        || (errno != 0 && val == 0)) {
+                perror("strtol");
+                exit(EXIT_FAILURE);
+        }
+        
+        if (endptr == str) {
+                fprintf(stderr, "No digits were found\n");
+                exit(EXIT_FAILURE);
+        }
+
+        /* Check for existing sign to set flag for proper function call */
+        if (argv[0] == '+') {
+                store->sign = '+';
+        } else if (argv[0] == '-') {
+                store->sign = '-';
+        }
+
+        /* Ensure value is within range */
+        if (val > 100) {
+                val = 100;
+        } else if (val < 1) {
+                val = 1;
+        }
+
+        store->brightness = val;
+
         if (*endptr != '\0'){        /* Not necessarily an error... */
                 printf("Further characters after number: %s\n", endptr);
         }
         
-        exit(EXIT_SUCCESS);
+        return(EXIT_SUCCESS);
 }
