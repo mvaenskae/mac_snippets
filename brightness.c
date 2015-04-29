d3d1272b0cd506695dc82c9a358cd5f4b33c835c
Mickey Vänskä <mvaenskae@gmail.com>
2015-04-29 10:07:34 +0200

    first version of the program setting values. still a mess in code quality though

--- brightness.c
+++ brightness.c
@@ -13,10 +13,12 @@ int main(int argc, char *argv[])
 {
         const int base = 10;
         char *endptr, *str;
-        const char *fp;
+        const char *p_brightness = "/sys/class/backlight/nv_backlight/brightness";
+        FILE *f_brightness = NULL;
+        char *c_brightness;
         long brightness = 0;
 
-        if (argc > 2) {
+        if (argc != 2) {
                 fprintf(stderr, "Usage: %s str\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
@@ -43,6 +45,48 @@ int main(int argc, char *argv[])
         } else if (brightness < 1) {
                 brightness = 1;
         }
+
+        /*
+        f_brightness = fopen(p_brightness, "rb");
+
+        if (!f_brightness) {
+                perror("fopen");
+                exit(EXIT_FAILURE);
+        }
+
+        c_brightness = calloc(3, sizeof(char));
+
+        size_t reading = fread(c_brightness, 1, 3, f_brightness);
+
+        if (!reading) {
+                perror("fread");
+                exit(EXIT_FAILURE);
+        }
+
+        printf("Read in 3 chars from file, got content: %s (would set to %ld)\n", c_brightness, brightness); 
+        free(c_brightness);
+        c_brigthness = NULL;
+        */
+        f_brightness = fopen(p_brightness, "wb");
+
+        if (!f_brightness) {
+                perror("fopen");
+                exit(EXIT_FAILURE);
+        }
+
+        //c_brightness = calloc(3, sizeof(char));
+        c_brightness = argv[1];
+
+        size_t writing = fwrite(c_brightness, 1, 3, f_brightness);
+
+        if (!writing) {
+                perror("fread");
+                exit(EXIT_FAILURE);
+        }
+
+        printf("Wrote new brightness value %s!\n", c_brightness);
+        //free(c_brightness);
+        //c_brightness = NULL;
 
         /* WRITE VALUE INSIDE FILE at /sys/class/backlight/nv_backlight/brightness */
         
